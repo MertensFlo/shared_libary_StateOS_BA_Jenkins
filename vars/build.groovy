6660b36c753fb0cf4ce701cd9fb3e703b28e8616
MertensFlo <fmertens20@gmail.com>
2024-07-24 14:31:01 +0200

    updated call

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,17 +1,14 @@
 #!/usr/bin/env groovy
+import 
 
 def call(){
-    script{
-        shared_steps.install_dependencies()
-        shared_steps.checkout()
-    }
+    install_dependencies()
+    checkout()
     sh'''
         apt-get -y -qq update && apt-get -y -qq install cmake ninja-build
     '''
-    script{
-        shared_steps.arm-none-eabi-gcc()
-        shared_steps.check_installs()
-    }
+    arm-none-eabi-gcc()
+    check_installs()
     sh '''
         cd StateOS_BA_Jenkins
         cmake -S. -Bbuild -GNinja
