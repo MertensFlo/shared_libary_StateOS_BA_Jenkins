06025ff35c7cc5a5bd487f2b1cafffc35ef02589
MertensFlo <fmertens20@gmail.com>
2024-07-24 14:48:30 +0200

    install of apt-utils added

--- vars/install_dependencies.groovy
+++ vars/install_dependencies.groovy
@@ -1,9 +1,10 @@
 def call(){
     sh '''
         apt-get update && apt-get -y -qq install git
-        apt-get update && apt-get install -y wget
-        apt-get update && apt-get install -y xz-utils 
-        apt-get update && apt-get install -y gcc-arm-none-eabi
-        apt-get update && apt-get install -y build-essential
+        apt-get apt-get install -y wget
+        apt-get apt-get install -y xz-utils 
+        apt-get apt-get install -y gcc-arm-none-eabi
+        apt-get apt-get install -y build-essential
+        apt-get install apt-utils
     '''
 }
