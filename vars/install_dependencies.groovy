9ea3ee54bbcad1cff7b9a16a7d43b3d97fe2173b
MertensFlo <fmertens20@gmail.com>
2024-07-24 14:51:22 +0200

    removed double apt-get

--- vars/install_dependencies.groovy
+++ vars/install_dependencies.groovy
@@ -1,10 +1,10 @@
 def call(){
     sh '''
         apt-get update && apt-get -y -qq install git
-        apt-get apt-get install -y wget
-        apt-get apt-get install -y xz-utils 
-        apt-get apt-get install -y gcc-arm-none-eabi
-        apt-get apt-get install -y build-essential
+        apt-get install -y wget
+        apt-get install -y xz-utils 
+        apt-get install -y gcc-arm-none-eabi
+        apt-get install -y build-essential
         apt-get install apt-utils
     '''
 }
