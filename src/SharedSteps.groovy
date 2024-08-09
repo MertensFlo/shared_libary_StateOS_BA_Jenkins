ae850ae9684859ad75b54f7522735b0600409f18
MertensFlo <fmertens20@gmail.com>
2024-08-09 11:09:34 +0200

    new labeling fix

--- src/SharedSteps.groovy
+++ src/SharedSteps.groovy
@@ -7,18 +7,18 @@ def checkout_git(){
         cd StateOS_BA_Jenkins
         git remote
         git submodule update --init --remote --recursive
-    ''' label: "git checkout"
+    ''', label: "git checkout"
 }
 
 def install_dependencies(){
-    sh '''
+    sh script:'''
         apt-get update && apt-get -y -qq install git
         apt-get install -y wget
         apt-get install -y xz-utils 
         apt-get install -y gcc-arm-none-eabi
         apt-get install -y build-essential
         apt-get install apt-utils
-    ''' label: "install dependencies"
+    ''', label: "install dependencies"
 }
 
 def arm_install(){
@@ -26,14 +26,14 @@ def arm_install(){
         wget -q https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz
         echo "1"
         tar -xf arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz
-    ''' label: "arm install"
+    ''', label: "arm install"
     script{
         env.JENKINS_PATH = "${env.WORKSPACE}/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin"
-    }  label: "set variable"
+    }
 }
 
 def check_installs(){
     sh '''
         arm-none-eabi-gcc -v
-    ''' label: "check installs"
+    ''', label: "check installs"
 }
