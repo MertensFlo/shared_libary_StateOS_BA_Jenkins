ac201bcc67bc7eecd02604983f6e65eda205a1d2
MertensFlo <fmertens20@gmail.com>
2024-08-10 13:48:05 +0200

    updating methode calls

--- src/StageTest.groovy
+++ src/StageTest.groovy
@@ -1,9 +1,9 @@
 
-def test(boolean example_test, boolean static_code, boolean unit_test, String workingRepo){
+def test(boolean example_test, boolean static_code, boolean unit_test, boolean arm, String arm_version, String workingRepo){
     def sharedSteps = new SharedSteps()
     //sharedSteps.install_ninja()
-    sharedSteps.install_pipeline_specific()
-    sharedSteps.check_installs()
+    sharedSteps.install_pipeline_specific(arm_version, arm)
+    sharedSteps.check_installs(arm)
     if(example_test){
         sharedSteps.example_test(workingRepo)
     }else if(static_code){
