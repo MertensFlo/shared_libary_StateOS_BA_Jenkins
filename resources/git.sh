05ae06588a3cf27e5cf25bf734a890222e0fa676
MertensFlo <fmertens20@gmail.com>
2024-08-10 10:37:37 +0200

    testing git with variables

--- resources/git.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-git --version
-echo $1
-echo $2
-echo "https://github.com/"$1"/"$2".git"
-git clone "https://github.com/"$1"/"$2".git"
-cd $workingRepo
-git remote
-git submodule update --init --remote --recursive
--- src/SharedSteps.groovy
+++ src/SharedSteps.groovy
@@ -2,43 +2,47 @@
 
 def checkout_git(String workingOrg, String workingRepo){
     sh script: """
-        echo "workingOrg: $workingOrg"
-        echo "workingOrg:" + workingOrg
+        git --version
+        echo workingOrg: $workingOrg
+        echo workingRepo: $workingRepo
+        echo "https://github.com/"$workingOrg"/"$workingRepo".git"
+        git clone "https://github.com/"$workingOrg"/"$workingRepo".git"
+        cd $workingRepo
+        git remote
+        git submodule update --init --remote --recursive
     """
-    def load_git_script = libraryResource 'git.sh'
-    bash load_git_script $workingOrg workingRepo
 }
 
 def install_dependencies(){
-    sh script: '''
-        apt-get update && apt-get -y -qq install git'''/*
+    sh script: """
+        apt-get update && apt-get -y -qq install git
         apt-get install -y wget
         apt-get install -y xz-utils 
         apt-get install -y gcc-arm-none-eabi
         apt-get install -y build-essential
         apt-get install apt-utils
-    ''', label: "install dependencies"*/
+    """, label: "install dependencies"
 }
 
 def install_ninja(){
-    sh script:'''
+    sh script:"""
         apt-get -y -qq update && apt-get -y -qq install cmake ninja-build
-    ''' , label:"install"
+    """ , label:"install"
 }
 
 def arm_install(){
-    sh script: '''
+    sh script: """
         wget -q https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz
         echo "1"
         tar -xf arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz
-    ''', label: "arm install"
+    """, label: "arm install"
     script{
         env.JENKINS_PATH = "${env.WORKSPACE}/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin"
     }
 }
 
 def check_installs(){
-    sh script: '''
+    sh script: """
         arm-none-eabi-gcc -v
-    ''', label: "check installs"
+    """, label: "check installs"
 }
