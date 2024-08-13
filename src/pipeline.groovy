8e3e19979006dfc67ac70f2a25db156356f1440e
MertensFlo <fmertens20@gmail.com>
2024-08-14 01:36:16 +0200

    Updated structure to accomedate scripted pipeline

--- src/pipeline.groovy
+++ src/pipeline.groovy
@@ -8,16 +8,16 @@ class Pipeline_ {
     void pipeline_build(Boolean build, Boolean example_test, Boolean static_test, Boolean unit_test){
         def stages = new Stages_(script)
         if(build){
-            stages.build()
+            stages.build("build")
         }
         if(example_test){
-            stages.example_test()
+            stages.example_test("example_test")
         }
         if(static_test){
-            stages.static_test()
+            stages.static_test("static_test")
         }
         if(unit_test){
-            stages.unit_test()
+            stages.unit_test("unit_test")
         }
     }
 }
