0cf5e4f15270fba317e0be365f275c3b505fa809
MertensFlo <fmertens20@gmail.com>
2024-08-14 02:43:03 +0200

    testing sudo install

--- src/SharedSteps.groovy
+++ src/SharedSteps.groovy
@@ -18,7 +18,9 @@ class SharedSteps {
     }
 
     def install_dependencies(boolean git, boolean wget, boolean xz_utils, boolean arm, boolean build_essential, boolean apt_utils, boolean ninja){
-        script.sh "apt install sudo"
+        script.sh """
+            su -
+            apt install sudo"""
         if(git){
             script.sh "sudo apt-get -y -qq install git"//, label: "install git"
         }
