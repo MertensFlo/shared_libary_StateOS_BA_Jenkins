50818d113ab2ae6e3bbdb8ccf1026404d63b7644
MertensFlo <fmertens20@gmail.com>
2024-08-14 02:51:26 +0200

    run pipeline with echos

--- src/SharedSteps.groovy
+++ src/SharedSteps.groovy
@@ -6,44 +6,40 @@ class SharedSteps {
     }
     def checkout_git(String workingOrg, String workingRepo){
         script.sh """
-            git --version
-            echo workingOrg: $workingOrg
-            echo workingRepo: $workingRepo
-            echo "https://github.com/"$workingOrg"/"$workingRepo".git"
-            git clone "https://github.com/"$workingOrg"/"$workingRepo".git"
-            cd $workingRepo
-            git remote
-            git submodule update --init --remote --recursive
+            echo git --version
+            echo git clone "https://github.com/"$workingOrg"/"$workingRepo".git"
+            echo cd $workingRepo
+            echo git remote
+            echo git submodule update --init --remote --recursive
         """
     }
 
     def install_dependencies(boolean git, boolean wget, boolean xz_utils, boolean arm, boolean build_essential, boolean apt_utils, boolean ninja){
-        script.sh "apt-get install sudo"
         if(git){
-            script.sh "sudo apt-get -y -qq install git"//, label: "install git"
+            script.sh "echo apt-get -y -qq install git"//, label: "install git"
         }
         if(wget){
-            script.sh "apt-get install -y wget"//, label: "install wget"
+            script.sh "echo apt-get install -y wget"//, label: "install wget"
         }
         if(xz_utils){
-            script.sh "apt-get install -y xz-utils"//, label: "install xz-utils"
+            script.sh "echo apt-get install -y xz-utils"//, label: "install xz-utils"
         }
         if(arm){
-            script.sh "apt-get install -y gcc-arm-none-eabi"//, label: "install arm"
+            script.sh "echo apt-get install -y gcc-arm-none-eabi"//, label: "install arm"
         }
         if(build_essential){
-            script.sh "apt-get install -y build-essential"//, label: "install build-essential"
+            script.sh "echo apt-get install -y build-essential"//, label: "install build-essential"
         }
         if(apt_utils){
-            script.sh "apt-get install apt-utils"//, label: "install apt-utils"
+            script.sh "echo apt-get install apt-utils"//, label: "install apt-utils"
         }
         if(ninja){
-            script.sh "apt-get -y -qq update && apt-get -y -qq install cmake ninja-build"//, label: "install ninja"
+            script.sh "echo apt-get -y -qq update && apt-get -y -qq install cmake ninja-build"//, label: "install ninja"
         }
     }
 
     def install_pipeline_specific(String arm_version, boolean arm){
-        if(arm){
+        if(arm && false){
             script.sh """
                 wget -q https://developer.arm.com/-/media/Files/downloads/gnu/$arm_version/binrel/arm-gnu-toolchain-$arm_version-x86_64-arm-none-eabi.tar.xz
                 echo "1"
@@ -54,21 +50,21 @@ class SharedSteps {
     }
 
     def check_installs(boolean arm){
-        if(arm){
+        if(arm && false){
             script.sh "arm-none-eabi-gcc -v"//, label: "check installs"
         }
     }
 
     def make_build(String workingRepo, boolean ninja){
-        if(ninja){
+        if(ninja && false){
             script.sh """
                 cd $workingRepo
                 cmake -S. -Bbuild -GNinja
             """//, label:"build ninja"
         }
         script.sh """
-            cd $workingRepo
-            cmake --build build -v  
+            echo cd $workingRepo
+            echo cmake --build build -v  
         """ //, label:"make build"
     }
 
