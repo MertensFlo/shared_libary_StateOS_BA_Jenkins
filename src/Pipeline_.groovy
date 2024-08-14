b84e844487492f0275fac4d90b2c5e54f3897cc3
MertensFlo <fmertens20@gmail.com>
2024-08-14 11:14:05 +0200

    testing this in constructor

--- src/Pipeline_.groovy
+++ src/Pipeline_.groovy
@@ -6,7 +6,7 @@ class Pipeline_{
     }
 
     void pipeline_build(Boolean flag_build, Boolean flag_example_test, Boolean flag_static_test, Boolean flag_unit_test, Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String arm_working_dir, String cmake_version){
-        def stages = new Stages_(script)
+        def stages = new Stages_(this)
         if(build){
             stages.build("build", flag_git, flag_wget, flag_xz_utils, flag_arm, flag_build_essential, flag_apt_utils, flag_ninja, flag_submodule, workingOrg, workingRepo, arm_path, arm_tar_path, arm_dir_path, arm_condition, arm_working_dir, cmake_version)  //(Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String cmake_version))
         }
--- src/StageBuild.groovy
+++ src/StageBuild.groovy
@@ -6,7 +6,7 @@ class StageBuild{
     }
 
     void build(Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String arm_working_dir, String cmake_version){
-        def sharedSteps = new SharedSteps(script)
+        def sharedSteps = new SharedSteps(this)
         sharedSteps.install_dependencies(flag_git, flag_wget, flag_xz_utils, flag_arm, flag_build_essential, flag_apt_utils, flag_ninja, workingRepo, cmake_version) //(Boolean git, Boolean wget, Boolean xz-utils, Boolean arm, Boolean build-essential, Boolean apt-utils)
         sharedSteps.checkout_git(workingOrg, workingRepo, flag_submodule) //(String workingOrg, String workingRepo, Boolean flag_submodule)
         sharedSteps.install_pipeline_specific(arm_path, arm_tar_path, arm_dir_path, arm_condition, arm_working_dir, flag_arm) //(String arm_path, String arm_tar_path,String arm_dir_path String arm_condition, String arm_working_dir, Boolean flag_arm)
