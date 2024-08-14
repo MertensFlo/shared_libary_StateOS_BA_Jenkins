cc20106d4a7977f1d4184a5731f30720f54f2ec0
MertensFlo <fmertens20@gmail.com>
2024-08-14 10:17:15 +0200

    Test fix of class not found

--- src/Stages_.groovy
+++ src/Stages_.groovy
@@ -1,28 +1,27 @@
 class Stages_{
     private final Script script
-    StageBuild buildStage
-    StageTest testStage
 
     Stages_(Script script) {
         this.script = script
-        this.buildStage = new StageBuild(script)
-        this.testStage = new StageTest(script)
     }
 
     void build(String stage_name, Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String cmake_version){
         script.stage(stage_name) {
+            buildStage = new StageBuild(script)
             buildStage.build( flag_git, flag_wget, flag_xz_utils, flag_arm, flag_build_essential, flag_apt_utils, flag_ninja, flag_submodule, workingOrg, workingRepo, arm_path, arm_tar_path, arm_dir_path, arm_condition, cmake_version)  //(Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String cmake_version)
         }
     }
 
     void example_test(String stage_name, Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo){
         script.stage(stage_name) {
+            testStage = new StageTest(script)
             testStage.test( flag_example_test, flag_static_code, flag_unit_test, flag_arm, arm_path, arm_tar_path, arm_dir_path, arm_condition, workingRepo) //(Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo)
         }
     }
 
     void static_code_test(String stage_name,Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo){
         script.stage(stage_name) {
+            testStage = new StageTest(script)
             testStage.test(fflag_example_test, flag_static_code, flag_unit_test, flag_arm, arm_path, arm_tar_path, arm_dir_path, arm_condition, workingRepo) //(Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo)
         }
         
@@ -30,6 +29,7 @@ class Stages_{
 
     void unit_test(String stage_name,Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo){
         script.stage(stage_name) {
+            testStage = new StageTest(script)
             testStage.test(flag_example_test, flag_static_code, flag_unit_test, flag_arm, arm_path, arm_tar_path, arm_dir_path, arm_condition, workingRepo) //(Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo)
         }
     }
