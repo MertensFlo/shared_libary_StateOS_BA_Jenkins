23eddc2f03d9633783965619538eac10c21c71f2
MertensFlo <fmertens20@gmail.com>
2024-08-14 10:23:39 +0200

    testing testStage

--- src/Stages_.groovy
+++ src/Stages_.groovy
@@ -6,10 +6,10 @@ class Stages_{
     }
 
     void build(String stage_name, Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String cmake_version){
-        script.stage(stage_name) {
+        /*script.stage(stage_name) {
             buildStage = new StageBuild_(script)
             buildStage.build( flag_git, flag_wget, flag_xz_utils, flag_arm, flag_build_essential, flag_apt_utils, flag_ninja, flag_submodule, workingOrg, workingRepo, arm_path, arm_tar_path, arm_dir_path, arm_condition, cmake_version)  //(Boolean flag_git, Boolean flag_wget, Boolean flag_xz_utils, Boolean flag_arm, Boolean flag_build_essential, Boolean flag_apt_utils, Boolean flag_ninja, Boolean flag_submodule, String workingOrg, String workingRepo, String arm_path, String arm_tar_path, String arm_dir_path, String arm_condition, String cmake_version)
-        }
+        }*/
     }
 
     void example_test(String stage_name, Boolean flag_example_test, Boolean flag_static_code, Boolean flag_unit_test, Boolean flag_arm, String arm_path, String arm_tar_path,String arm_dir_path, String arm_condition, String workingRepo){
